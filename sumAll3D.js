7d5ab929bf1ea4d4cedd60bf57152629e443a707
RiouC <riou.christophe@protonmail.com>
2021-02-26 17:22:31 +0100

    sumall3d : add sumall3d.js

---
+++ sumAll3D.js
@@ -0,0 +1,25 @@
+let tab = [
+  [
+    [1, 7, 3],
+    [11, 17, 7],
+    [-3, -5],
+    [5, 13],
+  ],
+  [
+    [2, 4, 6, 8, 10],
+    [1, 3, 5],
+  ],
+  [[0]],
+  [[0], [1], [2], [1]],
+];
+
+const adder = (arr) => {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+let res = 0;
+for (let i of tab) {
+    res += adder(i.map(adder));
+}
+
+console.log(res);
